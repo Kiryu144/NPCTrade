bb539e8fe064276d011b3db4e636d091fe4eacc9
Kiryu144 <123davidLP@gmail.com>
2018-06-17 23:26:49 +0200

    Added Citizens 'trader' trait

--- src/net/andrasia/kiryu144/npctrade/Main.java
+++ src/net/andrasia/kiryu144/npctrade/Main.java
@@ -1,12 +1,28 @@
 package net.andrasia.kiryu144.npctrade;
 
+import net.andrasia.kiryu144.npctrade.traits.Trader;
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.trait.TraitInfo;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
 
+    public static Plugin instance;
+
     public void onEnable() {
+        instance = this;
 
+        /* Initialize Citizens2 API */
+        if(getServer().getPluginManager().getPlugin("Citizens") == null || !getServer().getPluginManager().getPlugin("Citizens").isEnabled()) {
+            getLogger().log(Level.SEVERE, "Citizens 2.0 not found or not enabled");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+        /* Register traits */
+        CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(Trader.class));
     }
-
 
 }
---
+++ src/net/andrasia/kiryu144/npctrade/traits/Trader.java
@@ -0,0 +1,56 @@
+package net.andrasia.kiryu144.npctrade.traits;
+
+import net.andrasia.kiryu144.npctrade.Main;
+import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.api.persistence.Persist;
+import net.citizensnpcs.api.trait.Trait;
+import net.citizensnpcs.api.trait.TraitName;
+import net.citizensnpcs.api.util.DataKey;
+import org.bukkit.event.EventHandler;
+
+@TraitName("trader")
+public class Trader extends Trait {
+    public Trader() {
+        super("trader");
+    }
+
+    String tradeName = null;
+
+    public void load(DataKey key) {
+        tradeName = key.getString("tradeName");
+    }
+
+    public void save(DataKey key) {
+        key.setString("tradeName", tradeName);
+    }
+
+    @EventHandler
+    public void click(NPCRightClickEvent event){
+        if(event.getNPC() == this.getNPC()){
+            event.getClicker().sendMessage(String.valueOf(tradeName));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
